7a47c3ba35a6d3883da1df8d18b7f4b661a2f2cb
jhs512 <jangka512@gmail.com>
2023-09-25 16:31:42 +0900

    async 로 메일발송 빠르게

--- src/main/java/com/sbs/demo5/domain/email/service/EmailService.java
+++ src/main/java/com/sbs/demo5/domain/email/service/EmailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class EmailService {
     private final JavaMailSender mailSender;
 
+    @Async
     public void send(String to, String subject, String body) {
         if (to.endsWith("@test.com")) return;
 
