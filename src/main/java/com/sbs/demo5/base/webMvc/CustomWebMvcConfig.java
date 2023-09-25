03365f97a79ec7d1ff651b22b7991ef3d7e8fda2
jhs512 <jangka512@gmail.com>
2023-09-25 11:52:47 +0900

    실제 파일 저장

---
+++ src/main/java/com/sbs/demo5/base/webMvc/CustomWebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.sbs.demo5.base.webMvc;
+
+import com.sbs.demo5.base.app.AppConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomWebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/gen/**")
+                .addResourceLocations("file:///" + AppConfig.getGenFileDirPath() + "/");
+    }
+}
