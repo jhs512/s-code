7e2a3243e08129c2f8e9961de9f0199faeec922e
jhs512 <jangka512@gmail.com>
2023-10-26 08:48:28 +0900

    socat -t 0 option

--- zero_downtime_deploy.sh
+++ zero_downtime_deploy.sh
@@ -9,7 +9,7 @@ services=(
 SOCAT_PORT=8081
 SLEEP_DURATION=60
 
-current_service=$(ps aux | grep "socat TCP-LISTEN:$SOCAT_PORT" | grep -v grep | awk '{print $NF}')
+current_service=$(ps aux | grep "socat -t 0 TCP-LISTEN:$SOCAT_PORT" | grep -v grep | awk '{print $NF}')
 
 # 현재 서비스가 없으면 초기 서비스 설정
 if [[ -z "$current_service" ]]; then
@@ -17,7 +17,7 @@ if [[ -z "$current_service" ]]; then
   initial_name="s_code_1_1"
   initial_port="${services[$initial_name]}"
   docker run --name="$initial_name" -p "$initial_port":8080 -v /docker_projects/s_code_1/volumes/gen:/gen --restart unless-stopped -e TZ=Asia/Seoul --pull always -d ghcr.io/jhs512/s-code-1
-  nohup socat "TCP-LISTEN:$SOCAT_PORT,fork" "TCP:localhost:$initial_port" &
+  nohup socat -t 0 "TCP-LISTEN:$SOCAT_PORT,fork" "TCP:localhost:$initial_port" &
   echo "Initial service set up successfully!"
   exit 0
 fi
@@ -51,8 +51,8 @@ docker run --name="$next_name" -p "$next_port":8080 -v /docker_projects/s_code_1
 sleep "$SLEEP_DURATION"
 
 # 포트 전환
-kill -9 $(ps aux | grep "socat TCP-LISTEN:$SOCAT_PORT" | grep -v grep | awk '{print $2}')
-nohup socat "TCP-LISTEN:$SOCAT_PORT,fork" "TCP:localhost:$next_port" &
+kill -9 $(ps aux | grep "socat -t 0 TCP-LISTEN:$SOCAT_PORT" | grep -v grep | awk '{print $2}')
+nohup socat -t 0 "TCP-LISTEN:$SOCAT_PORT,fork" "TCP:localhost:$next_port" &
 
 # 현재 서비스 끄기
 docker stop "$current_name"
